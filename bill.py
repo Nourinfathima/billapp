106e90b1e2cd3f3941cf3953c54345a2361a2076
nourinfathima <nourinnourin36@gmail.com>
2022-11-04 01:36:13 +0530

    text

---
+++ bill.py
@@ -0,0 +1,27 @@
+while True:
+    print("select an option from the menu")
+    print("1. Tea:-")
+    print("2.Cofee:-")
+    print("3.Chips:-")
+    print("4.Sandwitch:-")
+    print("5.cake:-")
+    print("6.Generate  Bill:-")
+    print("7.Exit:-")
+
+    choice=int(input("Please enter your choice:-"))
+    if(choice==1):
+        print("Tea added")
+    elif(choice==2):
+        print("Cofee added")
+    elif(choice==3):
+        print("Chips added")
+    elif(choice==4):
+        print("Sandwitch added")
+    elif(choice==5):
+        print("Cake added")
+    elif(choice==6):
+        print("bill")
+    elif(choice==7):
+        break    
+
+
